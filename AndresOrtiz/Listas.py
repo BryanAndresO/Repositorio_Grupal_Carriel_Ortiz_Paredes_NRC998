218f893f5a600f29e71283c3d8ed4cb7b1b022b0
Bryan Andres <bryanfamiliat@gmail.com>
2023-05-17 10:29:42 -0500

    Mi código para presentar

---
+++ AndresOrtiz/Listas.py
@@ -0,0 +1,17 @@
+print("Hola Profesor, estoy programando en python")
+# Declaración de una lista
+lista = [1, 2, 3, 4, 5]
+
+# Acceder a un elemento de la lista
+print(lista[0]) # Imprime 1
+
+# Modificar un elemento de la lista
+lista[0] = 6
+
+# Agregar un elemento a la lista
+lista.append(7)
+
+# Eliminar un elemento de la lista
+lista.remove(2)
+
+print(lista) # Imprime [6, 3, 4, 5, 7]
