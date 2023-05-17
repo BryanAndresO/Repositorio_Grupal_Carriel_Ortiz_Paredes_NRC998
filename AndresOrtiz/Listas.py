7ac56139569f939967f99087771e5b7bdf23fd68
Bryan Andres <bryanfamiliat@gmail.com>
2023-05-17 10:31:48 -0500

    Actualización de mi código

--- AndresOrtiz/Listas.py
+++ AndresOrtiz/Listas.py
@@ -1,3 +1,8 @@
+#UNIVERSIDAD DE LAS FUERZAS ARMDAS "ESPE"
+#INTEGRANTES: CARRIEL PAMELA, ORTIZ BRYAN, PAREDES CAMILA
+#NRC: 9898
+#FECHA: 17/05/2023
+#EJERCICIOS LISTAS Y EJERCICIOS PILAS
 print("Hola Profesor, estoy programando en python")
 # Declaración de una lista
 lista = [1, 2, 3, 4, 5]
