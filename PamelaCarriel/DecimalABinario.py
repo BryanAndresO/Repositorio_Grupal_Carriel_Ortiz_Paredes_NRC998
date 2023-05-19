89b35584c78c82667c38b517272d8f722fea7290
Pamela Carriel <pjcarriel@espe.edu.ec>
2023-05-19 10:40:17 -0500

    Ejecución de código

--- PamelaCarriel/DecimalABinario.py
+++ PamelaCarriel/DecimalABinario.py
@@ -1,3 +1,7 @@
+#UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+#INTEGRANTES: CARRIEL PAMELA, ORTIZ BRYAN, PAREDES CAMILA
+#NRC: 9898
+#Convertir de un número decimal a Binario
 import decimal
 numero_decimal = decimal.Decimal(input("Ingrese un número decimal: "))
 binario = ""
