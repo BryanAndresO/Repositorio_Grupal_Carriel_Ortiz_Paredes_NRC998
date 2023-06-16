c4e2d50952b36c15fa1da6feaf038450d80c459e
Pamela Carriel <pjcarriel@espe.edu.ec>
2023-06-16 09:37:45 -0500

    Busqueda lienal

--- EJERCICIOS ALGORITMOS BÚSQUEDA/EJERCICIO 1.py
+++ EJERCICIOS ALGORITMOS BÚSQUEDA/EJERCICIO 1.py
@@ -9,12 +9,12 @@ def busqueda_lineal(lista, objetivo):
             return indice  # Devuelve la posición del número objetivo si se encuentra
     return -1  # Devuelve -1 si el número objetivo no está presente
 
-numero_objetivo = int(input("Ingrese el número que quiere buscar: "))  # Solicita al usuario el número objetivo
-lista = [2, 5, 8, 10, 13, 18, 20]  # Crea una lista de números enteros
+numero_busqueda = int(input("Ingrese el número que quiere buscar: "))  # Solicita al usuario el número objetivo
+lista = [6, 9, 12, 10, 13, 18, 20]  # Crea una lista de números enteros
 
-posicion = busqueda_lineal(lista, numero_objetivo)  # Realiza la búsqueda lineal en la lista
+posicion = busqueda_lineal(lista, numero_busqueda)  # Realiza la búsqueda lineal en la lista
 
 if posicion != -1:  # Si la posición es diferente de -1, el número objetivo se encontró en la lista
-    print("El número", numero_objetivo, "se encuentra en la posición", posicion)
+    print("El número", numero_busqueda, "se encuentra en la posición", posicion)
 else:
-    print("El número", numero_objetivo, "no está en la lista.")
+    print("El número", numero_busqueda, "no está en la lista.")
