d0c368ca63df27c657a13e6785322cc58c4cca90
Pamela Carriel <pjcarriel@espe.edu.ec>
2023-07-19 10:34:25 -0500

    Back

--- Backtracking/Ejercicio4.py
+++ Backtracking/Ejercicio4.py
@@ -4,24 +4,26 @@ NRC:9898
 Tema: Subconjuntos con Suma Exacta
 Fecha: miércoles 19 de julio del 2023
 """ 
-def encontrar_combinaciones_con_suma_exacta(nums, suma_objetivo):
-    def backtrack(inicio, suma_actual, combinacion_actual):
-        if suma_actual == suma_objetivo:
-            combinaciones.append(combinacion_actual[:])
+def encontrar_subconjuntos_con_suma_exacta(nums, suma_objetivo):
+    def backtrack(inicio, objetivo, subconjunto_actual):
+        if objetivo == 0:
+            subconjuntos.append(subconjunto_actual[:])
             return
         for i in range(inicio, len(nums)):
-            if suma_actual + nums[i] <= suma_objetivo:
-                combinacion_actual.append(nums[i])
-                backtrack(i, suma_actual + nums[i], combinacion_actual)
-                combinacion_actual.pop()
+            if objetivo - nums[i] >= 0:
+                subconjunto_actual.append(nums[i])
+                backtrack(i + 1, objetivo - nums[i], subconjunto_actual)
+                subconjunto_actual.pop()
 
-    combinaciones = []
+    subconjuntos = []
     nums.sort()
-    backtrack(0, 0, [])
-    return combinaciones
+    backtrack(0, suma_objetivo, [])
+    return subconjuntos
+
+# Ejemplo de uso:
 if __name__ == "__main__":
-    nums = [2, 4, 6, 3]
-    suma_objetivo = 6
-    resultado = encontrar_combinaciones_con_suma_exacta(nums, suma_objetivo)
-    print("Combinaciones con suma igual a", suma_objetivo, ":")
+    nums = [3, 1, 4, 2, 5]
+    suma_objetivo = 7
+    resultado = encontrar_subconjuntos_con_suma_exacta(nums, suma_objetivo)
+    print("Subconjuntos con suma igual a", suma_objetivo, ":")
     print(resultado)
