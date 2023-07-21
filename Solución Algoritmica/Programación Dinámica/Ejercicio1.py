b080905797045b3cc8d94f376e588732b7ee55be
Bryan Ortiz <bryanfamiliat@gmail.com>
2023-07-21 10:20:30 -0500

    a

--- Solución Algoritmica/Programación Dinámica/Ejercicio1.py
+++ Solución Algoritmica/Programación Dinámica/Ejercicio1.py
@@ -1,22 +1,32 @@
-def longitud_subsecuencia_comun_mas_larga(cadena1, cadena2):
-    m, n = len(cadena1), len(cadena2)
+def mochila_01_programacion_dinamica(valores, pesos, capacidad_mochila):
+    n = len(valores)
+    dp = [[0 for _ in range(capacidad_mochila + 1)] for _ in range(n + 1)]
 
-    # Creamos una matriz para almacenar los resultados intermedios
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-    # Rellenamos la matriz utilizando el algoritmo de programación dinámica
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if cadena1[i - 1] == cadena2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
+    for i in range(1, n + 1):
+        for capacidad_actual in range(1, capacidad_mochila + 1):
+            if pesos[i - 1] <= capacidad_actual:
+                dp[i][capacidad_actual] = max(dp[i - 1][capacidad_actual],
+                                              valores[i - 1] + dp[i - 1][capacidad_actual - pesos[i - 1]])
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                dp[i][capacidad_actual] = dp[i - 1][capacidad_actual]
 
-    # La longitud de la subsecuencia común más larga está almacenada en dp[m][n]
-    return dp[m][n]
+    # Reconstruir la solución óptima
+    capacidad_actual = capacidad_mochila
+    elementos_seleccionados = []
+    for i in range(n, 0, -1):
+        if dp[i][capacidad_actual] != dp[i - 1][capacidad_actual]:
+            elementos_seleccionados.append(i - 1)
+            capacidad_actual -= pesos[i - 1]
+
+    elementos_seleccionados.reverse()
+
+    return dp[n][capacidad_mochila], elementos_seleccionados
 
 # Ejemplo de uso:
-cadena1 = "ABCDGH"
-cadena2 = "AEDFHR"
-resultado = longitud_subsecuencia_comun_mas_larga(cadena1, cadena2)
-print("Longitud de la subsecuencia común más larga:", resultado)
+valores = [60, 100, 120]
+pesos = [10, 20, 30]
+capacidad_mochila = 50
+
+max_valor, elementos_seleccionados = mochila_01_programacion_dinamica(valores, pesos, capacidad_mochila)
+print("Valor máximo:", max_valor)
+print("Elementos seleccionados:", elementos_seleccionados)
